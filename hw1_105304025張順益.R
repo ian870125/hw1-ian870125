e15c07ea3b7a3f4e4eeeb23b0e050bacecbcba0a
ian870125 <ian9631408@gmail.com>
2019-03-19 15:00:48 +0800

    hw1 105304025

--- hw1_105304025張順益.R
+++ hw1_105304025張順益.R
@@ -1,7 +1,14 @@
 args = commandArgs(trailingOnly=TRUE)
-args[3]
-for (i in c(2,3)){
-  a<-round(max(df1[,i]),2)
-  print(a)
-}
 
+df1=read.csv(paste("example",args[2],sep="\\"))
+
+hw1=matrix(nrow=1,ncol=3)
+hw1[1,1]=args[2]
+for (i in 2:3){
+  a<-round(max(df1[,i]),2)
+  hw1[1,i]=a
+}
+colnames(hw1)=c("set","weight","height")
+
+write.csv(hw1,args[4])
+                                                                                                                                                                          
