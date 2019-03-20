b58c83564cc8cfb63f992d9736e5675c32e12b57
ian870125 <ian9631408@gmail.com>
2019-03-20 17:13:15 +0800

    hw1

--- hw1_105304025張順益.R
+++ hw1_105304025張順益.R
@@ -4,7 +4,6 @@ if(c("--input"%in%args & "--output"%in%args)==FALSE){
   stop("missing input or output")
 } else if (args[1]=="--input"){
   df1=read.csv(args[2])
-<<<<<<< HEAD
   a=strsplit(args[2],"/")[[1]]
   a=a[length(a)]
   a=sub(".csv","",a)
@@ -17,16 +16,6 @@ if(c("--input"%in%args & "--output"%in%args)==FALSE){
 w=round(max(df1$weight),2)
 h=round(max(df1$height),2)
 hw1=matrix(c(a,w,h),ncol=3)
-=======
-  a=args[2]
-}else if(args[1]=="--output"){
-  df1=read.csv(args[4])
-  a=args[4]
-}
-w=round(max(df1$weight),2)
-h=round(max(df1$height),2)
-hw1=matrix(c(a,w,h),nrow=1,ncol=3)
->>>>>>> 94d0fd10d6f2ae9e8b1dba9c63210a045734ee69
 
 colnames(hw1)=c("set","weight","height")
 if(args[1]=="--input"){
@@ -35,7 +24,3 @@ if(args[1]=="--input"){
   write.csv(hw1,args[2])
 }
 
-<<<<<<< HEAD
-=======
-                                                                                                                                                                          
->>>>>>> 94d0fd10d6f2ae9e8b1dba9c63210a045734ee69
